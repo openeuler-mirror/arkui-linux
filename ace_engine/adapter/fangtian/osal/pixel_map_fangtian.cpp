bea259dc7dbb2df26500c40736b7977bbdf2df15
abc12133 <jinguoen@huawei.com>
2023-07-31 15:30:34 +0800

    compile adapter
    Signed-off-by: abc12133 <jinguoen@huawei.com>

--- ace_engine/adapter/fangtian/osal/pixel_map_fangtian.cpp
+++ ace_engine/adapter/fangtian/osal/pixel_map_fangtian.cpp
@@ -13,81 +13,164 @@
  * limitations under the License.
  */
 
-#include "pixel_map_preview.h"
+#include "pixel_map_fangtian.h"
+
+#include <sstream>
 
 #include "base/image/pixel_map.h"
 #include "base/log/log_wrapper.h"
 #include "base/utils/utils.h"
+#include "pixel_map_manager.h"
 
 namespace OHOS::Ace {
+PixelFormat PixelMapFangtian::PixelFormatConverter(Media::PixelFormat pixelFormat)
+{
+    switch (pixelFormat) {
+        case Media::PixelFormat::RGB_565:
+            return PixelFormat::RGB_565;
+        case Media::PixelFormat::RGBA_8888:
+            return PixelFormat::RGBA_8888;
+        case Media::PixelFormat::BGRA_8888:
+            return PixelFormat::BGRA_8888;
+        case Media::PixelFormat::ALPHA_8:
+            return PixelFormat::ALPHA_8;
+        case Media::PixelFormat::RGBA_F16:
+            return PixelFormat::RGBA_F16;
+        case Media::PixelFormat::UNKNOWN:
+            return PixelFormat::UNKNOWN;
+        case Media::PixelFormat::ARGB_8888:
+            return PixelFormat::ARGB_8888;
+        case Media::PixelFormat::RGB_888:
+            return PixelFormat::RGB_888;
+        case Media::PixelFormat::NV21:
+            return PixelFormat::NV21;
+        case Media::PixelFormat::NV12:
+            return PixelFormat::NV12;
+        case Media::PixelFormat::CMYK:
+            return PixelFormat::CMYK;
+        default:
+            return PixelFormat::UNKNOWN;
+    }
+}
+
+AlphaType PixelMapFangtian::AlphaTypeConverter(Media::AlphaType alphaType)
+{
+    switch (alphaType) {
+        case Media::AlphaType::IMAGE_ALPHA_TYPE_UNKNOWN:
+            return AlphaType::IMAGE_ALPHA_TYPE_UNKNOWN;
+        case Media::AlphaType::IMAGE_ALPHA_TYPE_OPAQUE:
+            return AlphaType::IMAGE_ALPHA_TYPE_OPAQUE;
+        case Media::AlphaType::IMAGE_ALPHA_TYPE_PREMUL:
+            return AlphaType::IMAGE_ALPHA_TYPE_PREMUL;
+        case Media::AlphaType::IMAGE_ALPHA_TYPE_UNPREMUL:
+            return AlphaType::IMAGE_ALPHA_TYPE_UNPREMUL;
+        default:
+            return AlphaType::IMAGE_ALPHA_TYPE_UNKNOWN;
+    }
+}
+
 RefPtr<PixelMap> PixelMap::CreatePixelMap(void* rawPtr)
 {
-    return nullptr;
+    std::shared_ptr<Media::PixelMap>* pixmapPtr = reinterpret_cast<std::shared_ptr<Media::PixelMap>*>(rawPtr);
+    if (pixmapPtr == nullptr || *pixmapPtr == nullptr) {
+        LOGW("pixmap pointer is nullptr when CreatePixelMap.");
+        return nullptr;
+    }
+    return AceType::MakeRefPtr<PixelMapFangtian>(*pixmapPtr);
 }
 
 RefPtr<PixelMap> PixelMap::CreatePixelMapFromDataAbility(void* uniquePtr)
 {
-    return nullptr;
+    auto* pixmap = reinterpret_cast<Media::PixelMap*>(uniquePtr);
+    CHECK_NULL_RETURN(pixmap, nullptr);
+    return AceType::MakeRefPtr<PixelMapFangtian>(std::shared_ptr<Media::PixelMap>(pixmap));
+}
+
+int32_t PixelMapFangtian::GetWidth() const
+{
+    CHECK_NULL_RETURN(pixmap_, 0);
+    return pixmap_->GetWidth();
+}
+
+int32_t PixelMapFangtian::GetHeight() const
+{
+    CHECK_NULL_RETURN(pixmap_, 0);
+    return pixmap_->GetHeight();
+}
+
+const uint8_t* PixelMapFangtian::GetPixels() const
+{
+    CHECK_NULL_RETURN(pixmap_, nullptr);
+    return pixmap_->GetPixels();
+}
+
+PixelFormat PixelMapFangtian::GetPixelFormat() const
+{
+    CHECK_NULL_RETURN(pixmap_, PixelFormat::UNKNOWN);
+    return PixelFormatConverter(pixmap_->GetPixelFormat());
+}
+
+AlphaType PixelMapFangtian::GetAlphaType() const
+{
+    CHECK_NULL_RETURN(pixmap_, AlphaType::IMAGE_ALPHA_TYPE_UNKNOWN);
+    return AlphaTypeConverter(pixmap_->GetAlphaType());
+}
+
+int32_t PixelMapFangtian::GetRowBytes() const
+{
+    CHECK_NULL_RETURN(pixmap_, 0);
+    return pixmap_->GetRowBytes();
+}
+
+int32_t PixelMapFangtian::GetByteCount() const
+{
+    CHECK_NULL_RETURN_NOLOG(pixmap_, 0);
+    return pixmap_->GetByteCount();
+}
+
+void* PixelMapFangtian::GetPixelManager() const
+{
+    Media::InitializationOptions opts;
+    CHECK_NULL_RETURN_NOLOG(pixmap_, nullptr);
+    auto newPixelMap = Media::PixelMap::Create(*pixmap_, opts);
+    return reinterpret_cast<void*>(new Media::PixelMapManager(newPixelMap.release()));
+}
+
+void* PixelMapFangtian::GetRawPixelMapPtr() const
+{
+    CHECK_NULL_RETURN(pixmap_, nullptr);
+    return pixmap_.get();
+}
+
+std::string PixelMapFangtian::GetId()
+{
+    // using pixmap addr
+    CHECK_NULL_RETURN(pixmap_, "nullptr");
+    std::stringstream strm;
+    strm << pixmap_.get();
+    return strm.str();
+}
+
+std::string PixelMapFangtian::GetModifyId()
+{
+    return std::string();
+}
+
+std::shared_ptr<Media::PixelMap> PixelMapFangtian::GetPixelMapSharedPtr()
+{
+    return pixmap_;
 }
 
 RefPtr<PixelMap> PixelMap::ConvertSkImageToPixmap(
     const uint32_t* colors, uint32_t colorLength, int32_t width, int32_t height)
 {
-    return nullptr;
-}
-
-int32_t PixelMapPreview::GetWidth() const
-{
-    return -1;
-}
-
-int32_t PixelMapPreview::GetHeight() const
-{
-    return -1;
-}
-
-const uint8_t* PixelMapPreview::GetPixels() const
-{
-    return nullptr;
-}
-
-PixelFormat PixelMapPreview::GetPixelFormat() const
-{
-    return PixelFormat::UNKNOWN;
-}
-
-AlphaType PixelMapPreview::GetAlphaType() const
-{
-    return AlphaType::IMAGE_ALPHA_TYPE_UNKNOWN;
-}
-
-int32_t PixelMapPreview::GetRowBytes() const
-{
-    return 0;
-}
-
-int32_t PixelMapPreview::GetByteCount() const
-{
-    return 0;
-}
-
-void* PixelMapPreview::GetPixelManager() const
-{
-    return nullptr;
-}
-
-void* PixelMapPreview::GetRawPixelMapPtr() const
-{
-    return nullptr;
-}
-
-std::string PixelMapPreview::GetId()
-{
-    return "";
-}
-
-std::string PixelMapPreview::GetModifyId()
-{
-    return "";
+    Media::InitializationOptions opts;
+    opts.size.width = width;
+    opts.size.height = height;
+    opts.editable = true;
+    std::unique_ptr<Media::PixelMap> pixmap = Media::PixelMap::Create(colors, colorLength, opts);
+    CHECK_NULL_RETURN(pixmap, nullptr);
+    std::shared_ptr<Media::PixelMap> sharedPixelmap(pixmap.release());
+    return AceType::MakeRefPtr<PixelMapFangtian>(sharedPixelmap);
 }
 } // namespace OHOS::Ace
