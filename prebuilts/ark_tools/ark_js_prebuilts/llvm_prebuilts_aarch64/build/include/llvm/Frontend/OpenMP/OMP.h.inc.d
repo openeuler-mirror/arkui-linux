d8256d917807d2dab31cafeee627c9c854770170
xieyijun3 <xieyijun3@huawei.com>
2023-08-10 17:49:54 +0800

    rm unused deps Signed-off-by: x30034819 <xieyijun3@huawei.com>
    Signed-off-by: xieyijun3 <xieyijun3@huawei.com>

--- prebuilts/ark_tools/ark_js_prebuilts/llvm_prebuilts_aarch64/build/include/llvm/Frontend/OpenMP/OMP.h.inc.d
+++
@@ -1,1 +0,0 @@
-include/llvm/Frontend/OpenMP/OMP.h.inc: /home/zyk/llvm_code/toolchain/llvm-project/llvm/include/llvm/Frontend/Directive/DirectiveBase.td
--- prebuilts/ark_tools/ark_js_prebuilts/llvm_prebuilts_windows/build/include/llvm/Frontend/OpenACC/ACC.h.inc.d
+++
@@ -1,1 +0,0 @@
-include/llvm/Frontend/OpenACC/ACC.h.inc: /home/zyk/llvm_code/toolchain/llvm-project/llvm/include/llvm/Frontend/Directive/DirectiveBase.td
