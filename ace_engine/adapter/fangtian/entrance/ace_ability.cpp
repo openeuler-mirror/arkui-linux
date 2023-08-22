a4ccf04c24ad5422858a9dcb451c768a807c1f68
abc12133 <jinguoen@huawei.com>
2023-08-22 14:45:22 +0800

    enable decor
    Signed-off-by: abc12133 <jinguoen@huawei.com>

--- ace_engine/adapter/fangtian/entrance/ace_ability.cpp
+++ ace_engine/adapter/fangtian/entrance/ace_ability.cpp
@@ -113,7 +113,7 @@ AceAbility::AceAbility(const AceRunArgs& runArgs) : runArgs_(runArgs)
         LOGI("Initialize for current process.");
         Container::UpdateCurrent(INSTANCE_ID_PLATFORM);
     });
-        
+
     flutterView_ = Platform::FlutterAceView::CreateView(ACE_INSTANCE_ID);
     CHECK_NULL_VOID(flutterView_);
 
@@ -245,6 +245,12 @@ void AceAbility::InitEnv()
     }
     AceContainer::SetResourcesPathAndThemeStyle(ACE_INSTANCE_ID, runArgs_.systemResourcesPath,
         runArgs_.appResourcesPath, runArgs_.themeId, runArgs_.deviceConfig.colorMode);
+
+    auto controller = GetGlfwWindowController();
+    if (controller != nullptr && controller->IsDecorEnable()) {
+        LOGI("Container modal is enabled.");
+        container->SetWindowModal(WindowModal::CONTAINER_MODAL);
+    }
 
     auto view = new RSAceView(ACE_INSTANCE_ID);
     if (runArgs_.aceVersion == AceVersion::ACE_2_0) {
@@ -429,6 +435,14 @@ void AceAbility::SurfaceChanged(
     runArgs_.deviceConfig.density = resolution;
     runArgs_.deviceWidth = width;
     runArgs_.deviceHeight = height;
+
+    context->GetTaskExecutor()->PostSyncTask(
+        [container]() {
+            auto pipelineContext = container->GetPipelineContext();
+            CHECK_NULL_VOID(pipelineContext);
+            pipelineContext->ShowContainerTitle(true);
+        },
+        TaskExecutor::TaskType::UI);
 }
 
 void AceAbility::ReplacePage(const std::string& url, const std::string& params)
